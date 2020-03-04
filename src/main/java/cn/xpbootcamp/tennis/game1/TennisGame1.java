f5d2678aa20a23b1889d1bd3436249a6381584be
liaobin <liaobin@thoughtworks.com>
2020-03-04 16:38:38 +0800

    refactor: simple cleanup

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -4,71 +4,60 @@ import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int score1 = 0;
+    private int score2 = 0;
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals("player1"))
+            score1 += 1;
         else
-            m_score2 += 1;
+            score2 += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore = 0;
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
+        StringBuilder score = new StringBuilder();
+        int tempScore;
+        if (score1 == score2) {
+            switch (score1) {
                 case 0:
-                    score = "Love-All";
+                    score = new StringBuilder("Love-All");
                     break;
                 case 1:
-                    score = "Fifteen-All";
+                    score = new StringBuilder("Fifteen-All");
                     break;
                 case 2:
-                    score = "Thirty-All";
+                    score = new StringBuilder("Thirty-All");
                     break;
                 default:
-                    score = "Deuce";
+                    score = new StringBuilder("Deuce");
                     break;
 
             }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+        } else if (score1 >= 4 || score2 >= 4) {
+            int minusResult = score1 - score2;
+            if (minusResult == 1) score = new StringBuilder("Advantage player1");
+            else if (minusResult == -1) score = new StringBuilder("Advantage player2");
+            else if (minusResult >= 2) score = new StringBuilder("Win for player1");
+            else score = new StringBuilder("Win for player2");
         } else {
             for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
+                if (i == 1) tempScore = score1;
                 else {
-                    score += "-";
-                    tempScore = m_score2;
+                    score.append("-");
+                    tempScore = score2;
                 }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
+                if (tempScore == 0) {
+                    score.append("Love");
+                } else if (tempScore == 1) {
+                    score.append("Fifteen");
+                } else if (tempScore == 2) {
+                    score.append("Thirty");
+                } else if (tempScore == 3) {
+                    score.append("Forty");
                 }
             }
         }
-        return score;
+        return score.toString();
     }
 }
--- src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
@@ -3,18 +3,11 @@ package cn.xpbootcamp.tennis.game2;
 import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame2 implements TennisGame {
-    public int P1point = 0;
-    public int P2point = 0;
+    private int P1point = 0;
+    private int P2point = 0;
 
-    public String P1res = "";
-    public String P2res = "";
-    private String player1Name;
-    private String player2Name;
-
-    public TennisGame2(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
+    private String P1res = "";
+    private String P2res = "";
 
     public String getScore() {
         String score = "";
@@ -109,16 +102,16 @@ public class TennisGame2 implements TennisGame {
 
     }
 
-    public void P1Score() {
+    private void P1Score() {
         P1point++;
     }
 
-    public void P2Score() {
+    private void P2Score() {
         P2point++;
     }
 
     public void wonPoint(String player) {
-        if (player == "player1")
+        if (player.equals("player1"))
             P1Score();
         else
             P2Score();

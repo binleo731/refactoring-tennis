be02517eb6de8c8493c935ac3b10129e198ef8f3
liaobin <liaobin@thoughtworks.com>
2020-03-05 12:16:53 +0800

    refactor: Replace Nested Conditional with Guard Clauses

--- src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
@@ -70,19 +70,18 @@ public class TennisGame2 implements TennisGame {
             score = P1res + "-" + P2res;
         }
 
+        if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
+            return "Win for player1";
+        }
+        if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
+            return "Win for player2";
+        }
         if (P1point > P2point && P2point >= 3) {
-            score = "Advantage player1";
+            return "Advantage player1";
         }
 
         if (P2point > P1point && P1point >= 3) {
-            score = "Advantage player2";
-        }
-
-        if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
-            score = "Win for player1";
-        }
-        if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
-            score = "Win for player2";
+            return "Advantage player2";
         }
         return score;
     }

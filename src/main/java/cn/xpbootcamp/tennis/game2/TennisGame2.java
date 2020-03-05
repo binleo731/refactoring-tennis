a5edc992c770307c6c3b462ec426e888328d807a
liaobin <liaobin@thoughtworks.com>
2020-03-05 17:58:07 +0800

    refactor: delete duplicate code

--- src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
@@ -3,7 +3,7 @@ package cn.xpbootcamp.tennis.game2;
 import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame2 implements TennisGame {
-    public static final String PLAYER_NAME = "player1";
+    private static final String PLAYER_NAME = "player1";
     private int P1point = 0;
     private int P2point = 0;
 
@@ -11,64 +11,6 @@ public class TennisGame2 implements TennisGame {
     private String P2res = "";
 
     public String getScore() {
-        String score = "";
-        if (P1point == P2point && P1point < 4) {
-            if (P1point == 0)
-                score = "Love";
-            if (P1point == 1)
-                score = "Fifteen";
-            if (P1point == 2)
-                score = "Thirty";
-            score += "-All";
-        }
-        if (P1point == P2point && P1point >= 3)
-            score = "Deuce";
-
-        if (P1point > 0 && P2point == 0) {
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-
-            P2res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > 0 && P1point == 0) {
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-
-            P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
-
-        if (P1point > P2point && P1point < 4) {
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > P1point && P2point < 4) {
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            score = P1res + "-" + P2res;
-        }
 
         if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
             return "Win for player1";
@@ -79,11 +21,20 @@ public class TennisGame2 implements TennisGame {
         if (P1point > P2point && P2point >= 3) {
             return "Advantage player1";
         }
-
         if (P2point > P1point && P1point >= 3) {
             return "Advantage player2";
         }
-        return score;
+
+        if (P1point == P2point && P1point >= 3) return "Deuce";
+
+        if (P1point == P2point) return getPlayerScore(P1point) + "-All";
+
+        return getPlayerScore(P1point) + "-" + getPlayerScore(P2point);
+    }
+
+    private String getPlayerScore(int player) {
+        String[] score = {"Love", "Fifteen", "Thirty", "Forty"};
+        return score[player];
     }
 
     public void wonPoint(String player) {

47b6448dc9aa516b9ce33a868bca1bcf15a1cc4b
liaobin <liaobin@thoughtworks.com>
2020-03-05 11:57:37 +0800

    refactor: fix temporary Field

--- src/main/java/cn/xpbootcamp/tennis/game3/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/game3/TennisGame3.java
@@ -5,6 +5,7 @@ import cn.xpbootcamp.tennis.TennisGame;
 public class TennisGame3 implements TennisGame {
 
     private static final String DEUCE = "Deuce";
+    private static final String PLAYER_NAME = "player1";
     private int player1;
     private int player2;
     private String player1Name;
@@ -15,15 +16,29 @@ public class TennisGame3 implements TennisGame {
         this.player2Name = player2Name;
     }
 
-    public String getScore() {
-        if (player1 < 4 && player2 < 4 && !(player1 + player2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            return (player1 == player2) ? p[player1] + "-All" : p[player1] + "-" + p[player2];
+    public void wonPoint(String playerName) {
+        if (playerName.equals(PLAYER_NAME)) {
+            this.player1++;
         } else {
-            return (player1 == player2) ?
-                    DEUCE
-                    : getResult();
+            this.player2++;
         }
+    }
+
+    public String getScore() {
+        if (isNoPlayerWin()) {
+            return (player1 == player2) ? getPlayerScore(player1) + "-All" : getPlayerScore(player1) + "-" + getPlayerScore(player2);
+        } else {
+            return (player1 == player2) ? DEUCE : getResult();
+        }
+    }
+
+    private boolean isNoPlayerWin() {
+        return player1 < 4 && player2 < 4 && !(player1 + player2 == 6);
+    }
+
+    private String getPlayerScore(int player) {
+        String[] score = {"Love", "Fifteen", "Thirty", "Forty"};
+        return score[player];
     }
 
     private String getResult() {
@@ -33,12 +48,4 @@ public class TennisGame3 implements TennisGame {
     private String getPlayerName() {
         return player1 > player2 ? player1Name : player2Name;
     }
-
-    public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
-            this.player1 += 1;
-        else
-            this.player2 += 1;
-    }
-
 }
